024a691ab49447aa70e48a550bb4e3220da8adb3
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-07-14 14:02:38 +0300

    SQL written in every sql file

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 20, "M", "brown", "aggressive", 1),
+("Rowdy", 21, "F", "black", "chill", 0),
+("Tabitha", 22, "F", "brown", "aggressive", 0),
+("Sergeant Brown", 23, "M", "brown", "chill", 1),
+("Melissa", 24, "F", "black", "aggressive", 1),
+("Grinch", 25, "M", "brown", "chill", 0),
+("Wendy", 26, "F", "black", "aggressive", 0),
+(NULL, 27, "M", "brown", "chill", 1);
